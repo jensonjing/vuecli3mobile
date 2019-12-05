f18b31b5ae5ca6094c0ac270b4a15ed4c7bb314a
jensonjing <jensonjing@gmail.com>
2019-12-05 22:27:23 +0800

    增加eventBus配置

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index';
 import store from './store';
+import Bus from './store/bus';
 
 //引入第三方库
 import Vant from 'vant';
 import 'vant/lib/index.css';
 Vue.use(Vant);
+Vue.use(Bus);
 
 //引入接口文件
 import httpServe from './server/https';
